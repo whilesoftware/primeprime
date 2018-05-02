66adcb6fd4f496d59ec71f94067d5f9c2c2fe6be
yorktronic <ty.walker@gmail.com>
2018-05-02 11:39:54 -0700

    put plotting code into functions, created ipynb for testing, adding histogram plots

--- plot_primes.py
+++ plot_primes.py
@@ -5,18 +5,30 @@ import numpy as np
 
 # Data for plotting
 df = pd.read_csv('data/primes.txt', header=None, names=['n', 'prime', 'diff'])
-n = df['n']
-p = df['prime']
-diff = df['diff']
 
-# Note that using plt.subplots below is equivalent to using
-# fig = plt.figure() and then ax = fig.add_subplot(111)
-fig, ax = plt.subplots()
-ax.plot(n, diff)
+def plot(df):
+	n = df['n']
+	p = df['prime']
+	diff = df['diff']
 
-ax.set(xlabel='n', ylabel='difference from previous prime',
-       title='prime numbers')
-ax.grid()
+	# Note that using plt.subplots below is equivalent to using
+	# fig = plt.figure() and then ax = fig.add_subplot(111)
+	fig, ax = plt.subplots()
+	ax.plot(n, diff)
 
-fig.savefig("test.png")
-plt.show()
+	ax.set(xlabel='n', ylabel='difference from previous prime',
+	       title='prime numbers')
+	ax.grid()
+
+	fig.savefig("test.png")
+	plt.show()
+
+	return None
+
+def hist(df):
+	h = pd.DataFrame.hist(df, column='diff', bins=160)
+	fig = plt.figure()
+	fig.savefig("prime_diff_histogram.png")
+	
+	return None
+	
